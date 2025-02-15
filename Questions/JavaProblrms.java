1c21ab27f92d3e858940febda9565b1650b6e20c
Pratik Majage <89234809+PratikRameshMajage@users.noreply.github.com>
2025-02-15 23:33:30 +0530

    V25.02.15

--- Questions/JavaProblrms.java
+++ Questions/JavaProblrms.java
@@ -148,6 +148,31 @@ public class JavaProblrms {
 
         ThisInstance obj15 = new ThisInstance();
         obj15.base(2,4);
+
+        ThisMethord obj16 = new ThisMethord();
+        obj16.area(0.5,4,5);
+    }
+}
+
+class ThisMethord {
+    double area;
+    final double pi = 3.14;
+    // Area of Circle. -> 3.14*r*r
+    void area(double radius){
+        area = pi*(radius*radius);
+        System.out.println("\nArea of Circle: "+area);
+    }
+    // Area of Sphere -> 4*3.14*r*r
+    void area(double num, double radius){
+        area(3);
+        area = num*pi*(radius*radius);
+        System.out.println("\nArea of Sphere: "+area);
+    }
+    // Area of Triangle. -> 0.5*base*height
+    void area(double num,double base,double height){
+        area(4,3);
+        area = num*(base*height);
+        System.out.println("\nArea of Triangle: "+area+"\n");
     }
 }
 
